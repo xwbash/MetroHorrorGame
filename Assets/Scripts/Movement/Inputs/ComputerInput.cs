cee90d44a72fbede24f3124b946edf7f0ebaf8fa
$bash <mahlukat191@gmail.com>
2023-03-11 20:55:43 +0300

    Project first commit
    initilize commit,
    
    Movement system update

---
+++ Assets/Scripts/Movement/Inputs/ComputerInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Movement.Inputs
+{
+    public class ComputerInput : InputBase
+    {
+        public override void AssignEvent(UnityAction<Vector2> unityAction)
+        {
+            InputEvent.AddListener(unityAction);
+        }
+
+        public override void DeAssignEvent(UnityAction<Vector2> unityAction)
+        {
+            InputEvent.RemoveListener(unityAction);
+        }
+
+
+        private void Update()
+        {
+            var inputPosition = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            
+            if (inputPosition != Vector2.zero)
+            {
+                InputEvent.Invoke(inputPosition);    
+            }
+            
+        }
+    }
+}
